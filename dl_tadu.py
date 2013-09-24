a261c696d5eb12a6de04135b021ff26032ebb73f
Ubuntu12.04.3 <yw07@ubuntu.(none)>
2013-09-24 15:49:46 -0700

    add

--- dl_tadu.py
+++ dl_tadu.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+  iook['author'] = 
 # vim:fileencoding=utf-8
 
 import sys
@@ -6,17 +7,36 @@ import urllib.request
 import lxml.html
 import re
 
-def dl(url):
-  html = urllib.request.urlopen(url).read().decode()
-  print(html)
+def getBookInfo(str):
+  html = lxml.html.fromstring(str)
+  book = {}
+  book['title'] = html.cssselect('a[href^="/book/366292/"]')[0].get('alt')
+  book['author'] = html.cssselect('a[href^="/book/author/"]')[0].get('alt')
+
+  print("书名:", book['title'])
+  print("作者:", book['author'])
+  def getlink(a):
+    link = a.get('href')
+    return ("")
+  book['links'] = list(map(getlink, html.cssselect('a[href^="/book/366292"]')))
+def geturl(url):
+  response = urllib.request.urlopen(url)
+  return response.read().decode()
+
+def getpage(url, f, count):
+  html = geturl()
   html = lxml.html.fromstring(html)
   title = html.cssselect('head')[0][1].text
   patt = '(%\w+)+'
   content = html.cssselect('script')[7].text
   content = re.search(patt, content).group()
-  print(content)
+  #print(content)
   content = content.replace('%','\\').encode()
   content = content.decode('unicode_escape')
+  
+def dl(url):
+  book = getBookInfo(geturl(url))
+  fname = book['title']
   
   with open('a.txt', 'w') as f:
     f.write(title) 
